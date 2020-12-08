1a9e7292301d53df7753f3266563566aa5e96d09
kileean20 <kilean@lbl.gov>
2020-12-07 21:18:56 -0800

    MPSLoss

--- pyTorchTemplate/torch_templates.py
+++ pyTorchTemplate/torch_templates.py
@@ -503,8 +503,9 @@ def MSE(input, target, epsilon = 1.0e-6, size_average=None, reduce=None, reducti
 
 
 
-class MPELoss():
-    def __init__(self, p=4, root = True ):
+class MPELoss(_Loss):
+    def __init__(self, p=4, root = True):
+        super(RMSELoss, self).__init__()
         self.p = p
         self.root= root
 
